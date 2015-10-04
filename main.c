fef434320a8d4256d2864e4eab78b2241f639a9b
aniketmkulkarni <kaniket21@gmail.com>
2015-10-04 23:57:44 +0530

    Updated attributes, .gitignore, and next window screen.

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ncurses.h> 
-int main()
-{
- char mesg[]="Sorting Algorithms";		/* message to be appeared on the screen */
- char ch;
- int row,col;				/* to store the number of rows and *
-					 * the number of colums of the screen */
- initscr();				/* start the curses mode */
- getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
- mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
-                     		/* print the message at the center of the screen */
- mvprintw(LINES - 2, 0, "Press ENTER to continue, ESC to exit\n");
- ch = getch();
- if(ch == 'CR') {
- 	/* go to next screen window ... */
- }
- if(ch == 'ESC')	
- refresh();
- endwin();
+int main() {
+	char mesg[]="SORTING ALGORITHM DEMOS";
+	char ch;
+	int row,col;
+	initscr();
+	raw();
+	noecho();
+	start_color();
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	getmaxyx(stdscr,row,col);
+	mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+ 	mvchgat(row/2, (col-strlen(mesg))/2, -1, A_BLINK, 1, NULL);
+	attron(A_STANDOUT);
+	mvprintw(LINES - 2, 0, "Press 'y' to continue, 'n' to exit\n");
+	attroff(A_STANDOUT);
+	ch = getch();
+	if(ch == 'y') {
+ 	/* go to next screen window */
+		clear();
+		strcpy(mesg, "Which sorting algorithms to demonstrate?\n");
+		mvprintw(row / 4, (col - strlen(mesg)) / 2, "%s", mesg);
+		refresh();
+		getch();
+	}
+	else if(ch == 'n') {
+		clear();
+		exit(0);
+	}	
+	refresh();
+	endwin();
 
- return 0;
+ 	return 0;
 }
